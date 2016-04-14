e19c22e6e1905b2e879231d89f0c90fba783f30c
Branko Ajzele <ajzele@gmail.com>
2016-04-14 19:51:52 +0200

    Create structural-adapter-pattern.php

---
+++ structural-adapter-pattern.php
@@ -0,0 +1,64 @@
+<?php
+
+// Concrete implementation of Stripe payment provider
+class Stripe
+{
+
+    public function capturePayment($amount)
+    {
+        /* Implementation... */
+    }
+
+    public function authorizeOnlyPayment($amount)
+    {
+        /* Implementation... */
+    }
+
+    public function cancelAmount($amount)
+    {
+        /* Implementation... */
+    }
+}
+
+// Simple interface for PaymentService adapter
+interface PaymentService
+{
+    public function capture($amount);
+
+    public function authorize($amount);
+
+    public function cancel($amount);
+}
+
+// Implemenation of PaymentService adapter
+class StripePaymentServiceAdapter implements PaymentService
+{
+
+    private $stripe;
+
+    public function __construct(Stripe $stripe)
+    {
+        $this->stripe = $stripe;
+    }
+
+    public function capture($amount)
+    {
+        $this->stripe->capturePayment($amount);
+    }
+
+    public function authorize($amount)
+    {
+        $this->stripe->authorizeOnlyPayment($amount);
+    }
+
+    public function cancel($amount)
+    {
+        $this->stripe->cancelAmount($amount);
+    }
+}
+
+// Client
+$stripe = new StripePaymentServiceAdapter(new Stripe());
+$stripe->authorize(49.99);
+$stripe->capture(19.99);
+$stripe->cancel(9.99);
