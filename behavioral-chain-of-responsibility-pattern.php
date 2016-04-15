41c1368ab9d9f648dc9fd2f4c17a8d6b9b85609c
Branko Ajzele <ajzele@gmail.com>
2016-04-15 18:15:24 +0200

    Create behavioral-chain-of-responsibility-pattern.php

---
+++ behavioral-chain-of-responsibility-pattern.php
@@ -0,0 +1,55 @@
+<?php
+
+abstract class Logger
+{
+    private $next = null;
+
+    public function setNext(Logger $logger)
+    {
+        $this->next = $logger;
+        return $this->next;
+    }
+
+    final public function log($message)
+    {
+        $this->writeLog($message);
+
+        if ($this->next !== null) {
+            $this->next->log($message);
+        }
+    }
+
+    abstract protected function writeLog($message);
+}
+
+class EmailLogger extends Logger
+{
+    public function writeLog($message)
+    {
+        echo 'Logging to email: ' . $message;
+    }
+}
+
+class ErrorLogger extends Logger
+{
+    protected function writeLog($message)
+    {
+        echo 'Logging to stderr: ' . $message;
+    }
+}
+
+class StdoutLogger extends Logger
+{
+    protected function writeLog($message)
+    {
+        echo 'Logging to stdout: ' . $message;
+    }
+}
+
+// Client
+$logger = new StdoutLogger();
+
+$logger->setNext(new ErrorLogger())
+    ->setNext(new EmailLogger());
+
+$logger->log('Log triggered!');
