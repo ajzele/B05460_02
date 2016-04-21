6dd23db2c7aeea12bf8688155e8c7a54c6f51a3e
Branko Ajzele <ajzele@gmail.com>
2016-04-21 18:09:01 +0200

    Update creational-abstract-factory-pattern.php

--- creational-abstract-factory-pattern.php
+++ creational-abstract-factory-pattern.php
@@ -1,52 +1,33 @@
 <?php
 
-interface Button
-{
+interface Button {
     public function render();
 }
 
-// Abstract Factory [abstract class OR interface]
-interface GUIFactory
-{
+interface GUIFactory {
     public function createButton();
 }
 
-// Concrete Product [class]
-class SubmitButton implements Button
-{
-
-    public function render()
-    {
+class SubmitButton implements Button {
+    public function render() {
         echo 'Render Submit Button';
     }
 }
 
-// Concrete Product [class]
-class ResetButton implements Button
-{
-
-    public function render()
-    {
+class ResetButton implements Button {
+    public function render() {
         echo 'Render Reset Button';
     }
 }
 
-// Concrete Factory [class]
-class SubmitFactory implements GUIFactory
-{
-
-    public function createButton()
-    {
+class SubmitFactory implements GUIFactory {
+    public function createButton() {
         return new SubmitButton();
     }
 }
 
-// Concrete Factory [class]
-class ResetFactory implements GUIFactory
-{
-
-    public function createButton()
-    {
+class ResetFactory implements GUIFactory {
+    public function createButton() {
         return new ResetButton();
     }
 }
@@ -56,7 +37,6 @@ $submitFactory = new SubmitFactory();
 $button = $submitFactory->createButton();
 $button->render();
 
-// Client
 $resetFactory = new ResetFactory();
 $button = $resetFactory->createButton();
 $button->render();
