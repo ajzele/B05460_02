f58e52fc8fd949c9ca4a8162124208f8bb9d3e25
Branko Ajzele <ajzele@gmail.com>
2016-04-21 14:58:05 +0200

    Update behavioral-mediator-pattern.php

--- behavioral-mediator-pattern.php
+++ behavioral-mediator-pattern.php
@@ -1,177 +1,66 @@
 <?php
 
-class BookMediator
-{
-    private $authorObj;
-    private $titleObj;
+interface MediatorInterface {
+    public function fight();
+    public function talk();
+    public function registerA(ColleagueA $a);
+    public function registerB(ColleagueB $b);
+}
 
-    function __construct($author, $title)
-    {
-        $this->authorObj = new BookAuthorColleague($author, $this);
-        $this->titleObj = new BookTitleColleague($title, $this);
+class ConcreteMediator implements MediatorInterface {
+    protected $talk; // ColleagueA
+    protected $fight; // ColleagueB
+
+    public function registerA(ColleagueA $a) {
+        $this->talk = $a;
     }
 
-    function getAuthor()
-    {
-        return $this->authorObj;
+    public function registerB(ColleagueB $b) {
+        $this->fight = $b;
     }
 
-    function getTitle()
-    {
-        return $this->titleObj;
+    public function fight() {
+        echo 'fighting...';
     }
-    
-    // sync everything
-    function change(BookColleague $changingClassIn)
-    {
-        if ($changingClassIn instanceof BookAuthorColleague) {
-            if ('upper' == $changingClassIn->getState()) {
-                if ('upper' != $this->getTitle()->getState()) {
-                    $this->getTitle()->setTitleUpperCase();
-                }
-            } elseif ('lower' == $changingClassIn->getState()) {
-                if ('lower' != $this->getTitle()->getState()) {
-                    $this->getTitle()->setTitleLowerCase();
-                }
-            }
-        } elseif ($changingClassIn instanceof BookTitleColleague) {
-            if ('upper' == $changingClassIn->getState()) {
-                if ('upper' != $this->getAuthor()->getState()) {
-                    $this->getAuthor()->setAuthorUpperCase();
-                }
-            } elseif ('lower' == $changingClassIn->getState()) {
-                if ('lower' != $this->getAuthor()->getState()) {
-                    $this->getAuthor()->setAuthorLowerCase();
-                }
-            }
-        }
+
+    public function talk() {
+        echo 'talking...';
     }
 }
 
-abstract class BookColleague
-{
-    private $mediator;
+abstract class Colleague {
+    protected $mediator; // MediatorInterface
+    public abstract function doSomething();
+}
 
-    function __construct($mediator_in)
-    {
-        $this->mediator = $mediator_in;
+class ColleagueA extends Colleague {
+
+    public function __construct(MediatorInterface $mediator) {
+        $this->mediator = $mediator;
+        $this->mediator->registerA($this);
     }
 
-    function getMediator()
-    {
-        return $this->mediator;
+	public function doSomething() {
+        $this->mediator->talk();
+	}
+}
+
+class ColleagueB extends Colleague {
+
+    public function __construct(MediatorInterface $mediator) {
+        $this->mediator = $mediator;
+        $this->mediator->registerB($this);
     }
 
-    function changed($changingClassIn)
-    {
-        $this->getMediator()->titleChanged($changingClassIn);
+    public function doSomething() {
+        $this->mediator->fight();
     }
 }
 
-class BookAuthorColleague extends BookColleague
-{
-    private $author;
-    private $state;
+// Client
+$mediator = new ConcreteMediator();
+$talkColleague = new ColleagueA($mediator);
+$fightColleague = new ColleagueB($mediator);
 
-    function __construct($author_in, $mediator_in)
-    {
-        $this->author = $author_in;
-        parent::__construct($mediator_in);
-    }
-
-    function getAuthor()
-    {
-        return $this->author;
-    }
-
-    function setAuthor($author_in)
-    {
-        $this->author = $author_in;
-    }
-
-    function getState()
-    {
-        return $this->state;
-    }
-
-    function setState($state_in)
-    {
-        $this->state = $state_in;
-    }
-
-    function setAuthorUpperCase()
-    {
-        $this->setAuthor(strtoupper($this->getAuthor()));
-        $this->setState('upper');
-        $this->getMediator()->change($this);
-    }
-
-    function setAuthorLowerCase()
-    {
-        $this->setAuthor(strtolower($this->getAuthor()));
-        $this->setState('lower');
-        $this->getMediator()->change($this);
-    }
-}
-
-class BookTitleColleague extends BookColleague
-{
-    private $title;
-    private $state;
-
-    function __construct($title_in, $mediator_in)
-    {
-        $this->title = $title_in;
-        parent::__construct($mediator_in);
-    }
-
-    function getTitle()
-    {
-        return $this->title;
-    }
-
-    function setTitle($title_in)
-    {
-        $this->title = $title_in;
-    }
-
-    function getState()
-    {
-        return $this->state;
-    }
-
-    function setState($state_in)
-    {
-        $this->state = $state_in;
-    }
-
-    function setTitleUpperCase()
-    {
-        $this->setTitle(strtoupper($this->getTitle()));
-        $this->setState('upper');
-        $this->getMediator()->change($this);
-    }
-
-    function setTitleLowerCase()
-    {
-        $this->setTitle(strtolower($this->getTitle()));
-        $this->setState('lower');
-        $this->getMediator()->change($this);
-    }
-}
-
-$mediator = new BookMediator('Branko A.', 'Magento 2 Developer\'s guide');
-
-$author = $mediator->getAuthor();
-$title = $mediator->getTitle();
-
-echo $author->getAuthor();
-echo $title->getTitle();
-
-$author->setAuthorLowerCase();
-echo $author->getAuthor();
-echo $title->getTitle();
-
-$title->setTitleUpperCase();
-echo $author->getAuthor();
-echo $title->getTitle();
+$talkColleague->doSomething();
+$fightColleague->doSomething();
